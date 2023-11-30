d6b9187110d8e10c5ab3740e101a1f890080ba69
Anders Ödlund <anders@giddy.co>
2023-11-29 16:54:18 -0800

    Initial commit

---
+++ lib/flutter_bitcoin.dart
@@ -0,0 +1,16 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_bitcoin;
+
+export 'src/bitcoin_flutter_base.dart';
+export 'src/models/networks.dart';
+export 'src/transaction.dart';
+export 'src/address.dart';
+export 'src/transaction_builder.dart';
+export 'src/ecpair.dart';
+export 'src/payments/p2pkh.dart';
+export 'src/payments/p2wpkh.dart';
+export 'src/payments/index.dart';
+export 'src/utils/script.dart' hide ZERO;
+// TODO: Export any libraries intended for clients of this package.
